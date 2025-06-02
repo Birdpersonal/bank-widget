510e63cb44a72ac0310a2094e6694656ed15b719
Vadim Retivov <vadik.eoe@mail.ru>
2025-06-02 18:29:03 +0300

    add mask account card

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,6 @@
+import re
+
+
 def get_mask_card_number(card_number: int) -> str:
     """принимает на вход номер карты и возвращает ее маску."""
     str_card_number = str(card_number)
@@ -15,3 +18,26 @@ def get_mask_account(account_number: int) -> str:
 
 
 # print(get_mask_account())
+
+def is_cirillic(mem: str) -> bool:
+    return bool(re.search("[а-яА-Я]", mem))
+
+
+def mask_account_card(card_type_and_number: str) -> str:
+    """Функция принимает тип и номер карты возвращает с замаскированными номером"""
+    card_type = ""
+    number_card = ""
+    for i in card_type_and_number:
+        for item in i:
+            if not item.isdigit():
+                card_type += item
+            else:
+                number_card += item
+    new_card_type = ""
+    for mem in card_type:
+        if  is_cirillic(mem):
+
+            return card_type + get_mask_account(number_card)
+        else:
+
+            return card_type + get_mask_card_number(number_card)
---
+++ src/widget.py
@@ -0,0 +1,13 @@
+from src.masks import mask_account_card
+
+card_type_and_number = mask_account_card("Visa Platinum 7000792289606361")
+print(card_type_and_number)
+
+
+
+
+
+
+
+
+
