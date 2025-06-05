53605112fa5f4b699e0f08b5ce74072721fe3e8b
Vadim Retivov <vadik.eoe@mail.ru>
2025-06-05 10:16:47 +0300

    eliminating homework issues

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_dick: list, state="EXECUTED") -> list:
+def filter_by_state(list_dick: list, state: str = "EXECUTED") -> list:
     """Функция принимает список словарей. Возвращает новый список,
     содержащий только те словари, у которых ключ state соответствует указанному значению"""
     new_list = []
@@ -14,9 +14,9 @@ def filter_by_state(list_dick: list, state="EXECUTED") -> list:
 # print(filter_by_state_fon)
 
 
-def sort_by_date(list_dicktionary: list, reverse=True) -> list:
+def sort_by_date(list_dicktionary: list, reverse: bool = True) -> list:
     """Функция принимает список словарей, возвращать новый список, отсортированный по дате"""
-    if reverse == True:
+    if reverse:
         return sorted(list_dicktionary, key=lambda x: x["date"], reverse=True)
     else:
         return sorted(list_dicktionary, key=lambda x: x["date"], reverse=False)
