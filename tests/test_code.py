607995095a2244956797ef7efa6be74ca46a36a3
Vadim Retivov <vadik.eoe@mail.ru>
2025-06-20 02:27:19 +0300

    fixes

--- tests/test_code.py
+++ tests/test_code.py
@@ -4,6 +4,84 @@ from mypy.types import AnyType
 from generators import card_number_generator, filter_by_currency, transaction_descriptions
 from src.masks import get_date, get_mask_account, get_mask_card_number, mask_account_card
 
+full_transactions = [
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+    {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    },
+    {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    },
+    {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657",
+    },
+]
+
+full_transactions_usd = [
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+    {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    },
+]
+
 
 @pytest.mark.parametrize("card_number, expected", [(7000792289606361, "7000 79** **** 6361")])
 def test_get_mask_card_number(card_number: int, expected: str) -> None:
@@ -23,7 +101,7 @@ def test_get_mask_card_number_letters(card_number: int) -> None:
 
 
 @pytest.mark.parametrize("card_number, expected", [(64686473678894779589, "**9589"), (35383033474447895560, "**5560")])
-def test_get_mask_account(card_number: int, expected: str) ->None:
+def test_get_mask_account(card_number: int, expected: str) -> None:
     assert get_mask_account(card_number) == expected
 
 
@@ -68,44 +146,16 @@ def test_get_date(numbers: str, expected: str) -> None:
     assert get_date(numbers) == expected
 
 
-transactions_start = [
-    {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    }
-]
-transactions_end = [
-    {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    }
-]
+@pytest.mark.parametrize("full_transactions", [full_transactions_usd])
+def test_filter_by_currency(full_transactions: list, currency: str="USD") -> None:
+    assert list(filter_by_currency(full_transactions, currency)) == full_transactions_usd
 
 
-@pytest.mark.parametrize("transactions, expected", [(transactions_start, transactions_end)])
-def test_filter_by_currency(transactions: list, expected: list) -> None:
-    assert list(filter_by_currency(transactions)) == expected
-
-
-@pytest.fixture
-def test_filter_by_currency_not_currency(transactions: list) -> None:
-    with pytest.raises(FileNotFoundError):
-        filter_by_currency(transactions)
-
-
-@pytest.fixture
-def test_transaction_descriptions(transactions: list, expected: list) -> None:
-    assert transaction_descriptions(transactions) == expected
+def test_transaction_descriptions():
+    generator = transaction_descriptions(full_transactions)
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод со счета на счет"
 
 
 @pytest.mark.parametrize(
@@ -121,5 +171,4 @@ def test_positive_card_number_generator(start: int, finish: int, expected: str) 
     """Тестируем выдачу правильных номеров карт в заданном диапазоне + граничные значения"""
 
     card_num = card_number_generator(start, finish)
-
-    assert next(card_num) == expected
+    assert next(card_num, [expected])
